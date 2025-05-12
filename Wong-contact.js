8850a6b46ad2fa860c1e72b540406a73cdf4d4ac
NolanWong <nolancwong@gmail.com>
2025-05-12 07:29:48 -0500

    Update Wong-contact.js

--- Wong-contact.js
+++ Wong-contact.js
@@ -4,12 +4,12 @@ let usrName=document.querySelector(".usrName").value;
 let thank=document.querySelector(".thank");
 let nav3=document.querySelector(".navbar-con3");
 let eMail=document.querySelector(".eMail").value;
-
+// Displays thanks message and resets navbar
 submitButton.addEventListener("click", function() {
 let eMail_value=eMail.value;
 let messages_value=messages.value;
 let usrName_value=usrName.value
 thank.style.display="block"
 console.log(usrName_value)
-nav3.style.marginTop="17.4%"
+nav3.style.marginTop="16.5%"
 });
